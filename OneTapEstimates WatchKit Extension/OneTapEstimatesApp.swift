df0041851b2191f65adb7164ec0c65517811a2f9
Neetesh Gupta <neetesh.ng@gmail.com>
2021-12-20 08:23:53 +0000

    Initial commit

---
+++ OneTapEstimates WatchKit Extension/OneTapEstimatesApp.swift
@@ -0,0 +1,19 @@
+//
+//  OneTapEstimatesApp.swift
+//  OneTapEstimates WatchKit Extension
+//
+//  Created by Neetesh Gupta on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct OneTapEstimatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ OneTapEstimatesTests/OneTapEstimatesTests.swift
@@ -0,0 +1,33 @@
+//
+//  OneTapEstimatesTests.swift
+//  OneTapEstimatesTests
+//
+//  Created by Neetesh Gupta on 20/12/2021.
+//
+
+import XCTest
+@testable import OneTapEstimates_WatchKit_Extension
+
+class OneTapEstimatesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
