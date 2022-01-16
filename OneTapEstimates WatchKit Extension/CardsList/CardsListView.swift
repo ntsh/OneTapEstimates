b6ab90078b797c1f93989e5aef2c5dd48a99c6cf
Neetesh Gupta <neetesh.ng@gmail.com>
2022-01-16 07:32:49 +0000

    Accessibility: Dynamic grid view columns based on font size

--- OneTapEstimates WatchKit Extension/CardsList/CardsListView.swift
+++ OneTapEstimates WatchKit Extension/CardsList/CardsListView.swift
@@ -11,10 +11,11 @@ import EstimatesKit
 struct CardsListView: View {
 
     @State var estimate: Estimate
+    @Environment(\.dynamicTypeSize) var typeSize: DynamicTypeSize
 
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 2)) {
+            LazyVGrid(columns: Array(repeating: .init(.flexible()), count: typeSize > .xxxLarge ? 1 : 2)) {
                 ForEach(estimate.values, id: \.self)  { card in
                     NavigationLink(destination: CardView(card: card)) {
                         Text("\(card)")
@@ -33,5 +34,7 @@ struct CardsListView_Previews: PreviewProvider {
 
     static var previews: some View {
         CardsListView(estimate: estimate)
+        CardsListView(estimate: estimate)
+            .environment(\.dynamicTypeSize, .accessibility5)
     }
 }
